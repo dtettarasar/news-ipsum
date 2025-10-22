9d2db66cf098449acd9a7b24c6e63c259cbd7765
Dylan Tettarasar <dtettarasar@gmail.com>
2025-10-22 14:46:59 +0200

    add tailwind, mongodb & mongoose: created basic view and process to connect to database

---
+++ nuxt-app/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline" >Test MongoDB</h1>
+    <button @click="testMongo">Tester</button>
+    <pre>{{ response }}</pre>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const response = ref('')
+
+async function testMongo() {
+  const res = await $fetch('/api/test-mongo') // notre endpoint server
+  response.value = JSON.stringify(res, null, 2)
+}
+</script>
