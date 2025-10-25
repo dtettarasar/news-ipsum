c9ce7b0bd54a1633f9a66df83677ce84e01b4ac9
Dylan Tettarasar <dtettarasar@gmail.com>
2025-10-25 17:15:15 +0200

    building navbar component

--- nuxt-app/components/layout/nav-bar.vue
+++ nuxt-app/components/layout/nav-bar.vue
@@ -1,6 +1,36 @@
 <template>
 
-    <p>This is the navbar component</p>
+    <div class="bg-blue-200 flex flex-row">
+
+        <div class="hidden lg:flex">
+
+            <!--Menu desktop-->
+
+            <div>
+                Logo section
+            </div>
+
+            <div>
+                menu section
+            </div>
+
+            <div> 
+                user section
+            </div>
+
+        </div>
+
+        <div class="flex lg:hidden">
+
+            <!--Menu mobile-->
+
+            menu mobile to do
+
+        </div>
+
+        
+
+    </div>
 
 </template>
 
