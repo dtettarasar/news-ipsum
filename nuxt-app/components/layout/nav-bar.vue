4638284aa61a3387e6f0cbc80ac3adb2fdcf948a
Dylan Tettarasar <dtettarasar@gmail.com>
2025-10-25 17:32:23 +0200

    add logos

--- nuxt-app/components/layout/nav-bar.vue
+++ nuxt-app/components/layout/nav-bar.vue
@@ -2,12 +2,12 @@
 
     <div class="border-t-1 border-b-1 px-4 bg-blue-200 flex flex-row">
 
-        <div class="hidden lg:flex flex-row basis-full justify-between">
+        <div class="hidden lg:flex flex-row basis-full justify-between items-center">
 
             <!--Menu desktop-->
 
             <div>
-                Logo section
+                <img src="/logos/logoipsum-381.svg" alt="Logo du site" class="h-8 w-auto" />
             </div>
 
             <div>
