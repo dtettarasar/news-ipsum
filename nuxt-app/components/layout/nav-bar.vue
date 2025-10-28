671f49932b1616d416e4ee89d385975c3b31afbc
Dylan Tettarasar <dtettarasar@gmail.com>
2025-10-28 12:03:13 +0100

    navbar component: translate style written in the component to tailwind style

--- nuxt-app/components/layout/nav-bar.vue
+++ nuxt-app/components/layout/nav-bar.vue
@@ -12,24 +12,24 @@
 
             <div class="flex flex-row justify-center items-center gap-8 h-full">
                 
-                <div class="menu-item-container flex flex-col justify-between items-center">
+                <div class="menu-item-container mt-[20px] h-[40px] flex flex-col justify-between items-center group">
                     <a href="/" class="">Home</a>
-                    <div class="rectangle"></div>
+                    <div class="rectangle w-[30px] h-[4px] rounded-t-full bg-transparent group-hover:bg-black transition-all duration-300"></div>
                 </div>
 
-                <div class="menu-item-container flex flex-col justify-between items-center">
+                <div class="menu-item-container mt-[20px] h-[40px] flex flex-col justify-between items-center group">
                     <a href="/about" class="">About</a>
-                    <div class="rectangle"></div>
+                    <div class="rectangle w-[30px] h-[4px] rounded-t-full bg-transparent group-hover:bg-black transition-all duration-300"></div>
                 </div>
                 
-                <div class="menu-item-container flex flex-col justify-between items-center">
+                <div class="menu-item-container mt-[20px] h-[40px] flex flex-col justify-between items-center group">
                     <a href="/contact" class="">Contact</a>
-                    <div class="rectangle"></div>
+                    <div class="rectangle w-[30px] h-[4px] rounded-t-full bg-transparent group-hover:bg-black transition-all duration-300"></div>
                 </div>
 
-                <div class="menu-item-container flex flex-col justify-between items-center">
+                <div class="menu-item-container mt-[20px] h-[40px] flex flex-col justify-between items-center group">
                     <a href="#" class="">All articles</a>
-                    <div class="rectangle"></div>
+                    <div class="rectangle w-[30px] h-[4px] rounded-t-full bg-transparent group-hover:bg-black transition-all duration-300"></div>
                 </div>
 
             </div>
@@ -61,17 +61,5 @@
 </script>
 
 <style scoped>
-
-    .rectangle {
-        width: 30px;
-        height: 4px;
-        background-color: black;
-        border-radius: 200px 200px 0% 0%
-    }
-
-    .menu-item-container {
-        height: 50px;
-        margin-top: 30px;
-    }
 
 </style>
