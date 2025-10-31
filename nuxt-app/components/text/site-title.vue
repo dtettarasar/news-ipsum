e40cdb45c3bf10246194fd3210a9331705e71bbe
Dylan Tettarasar <dtettarasar@gmail.com>
2025-10-31 15:21:52 +0100

    add dompurify + create site title component

---
+++ nuxt-app/components/text/site-title.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container mx-auto flex justify-center pt-[50px] lg:pt-[100px]">
+    <h1 class="text-3xl font-bold text-center">
+      {{ title }}
+    </h1>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const title = ref('Site Title') // valeur temporaire pour le front
+</script>
--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -1,10 +1,7 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline" >Home</h1>
-
-    <icon name="uil:github" class="w-8 h-8 text-black hover:text-sky-500 transition-colors" />
-
-    <other-test-mongo-db></other-test-mongo-db>
+    
+    <text-site-title></text-site-title>
 
   </div>
 </template>
