d5f94f1fdecc75e8d0ca2dfffbd38f7c12bafc08
Dylan Tettarasar <dtettarasar@gmail.com>
2025-10-31 15:33:42 +0100

    site title component: pass text value from variable and render it in HTML

--- nuxt-app/components/text/site-title.vue
+++ nuxt-app/components/text/site-title.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="container mx-auto flex justify-center pt-[50px] lg:pt-[100px]">
-    <h1 class="text-3xl font-bold text-center">
-      {{ title }}
-    </h1>
+    <h1 v-html="title" class="text-3xl font-bold text-center"></h1>
   </div>
 </template>
 
 <script setup>
+
+import DOMPurify from 'dompurify'
 import { ref } from 'vue'
 
-const title = ref('Site Title') // valeur temporaire pour le front
+const title = ref(DOMPurify.sanitize('<strong>Hello</strong> world'))
+
 </script>
