caeed9e6d211daff8372d6c5ffe7da9da725157d
Dylan Tettarasar <dtettarasar@gmail.com>
2025-12-21 14:27:14 +0100

    app.vue : add the instruction to handle the seo index

--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -1,3 +1,18 @@
+<script setup>
+const config = useRuntimeConfig()
+const isSeoEnabled = config.public.seoIndex
+
+useHead({
+  // Si isSeoEnabled est false, on injecte la balise noindex
+  meta: [
+    {
+      name: 'robots',
+      content: isSeoEnabled ? 'index, follow' : 'noindex, nofollow'
+    }
+  ]
+})
+</script>
+
 <template>
   <div>
 
