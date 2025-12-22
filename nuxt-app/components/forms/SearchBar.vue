8334672efb368ee41e19cffe594cc7fa09a35d81
Dylan Tettarasar <dtettarasar@gmail.com>
2025-12-22 17:12:31 +0100

    search bar component : small design adjustment + added animations

--- nuxt-app/components/forms/SearchBar.vue
+++ nuxt-app/components/forms/SearchBar.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="w-full md:w-[70%] lg:w-[50%] md:mx-auto px-4 md:px-0">
     <form @submit.prevent="handleSearch" 
-      class="flex flex-col md:flex-row w-full md:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] md:rounded-lg overflow-hidden md:border-2 md:border-black"
+      class="flex flex-col md:flex-row w-full transition-all duration-200
+             md:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] md:rounded-lg overflow-hidden md:border-2 md:border-black
+             active:md:translate-x-[2px] active:md:translate-y-[2px] active:md:shadow-none"
     >
       
       <div class="relative flex-grow">
@@ -14,9 +16,16 @@
 
       <button 
         type="submit" 
-        class="mt-4 md:mt-0 bg-[#40E0D0] border-2 border-black rounded-lg md:rounded-none md:border-l-2 md:border-y-0 md:border-r-0 flex items-center justify-center p-4 transition-colors hover:bg-turquoise-dark active:translate-y-[2px] active:translate-x-[2px] active:shadow-none"
+        class="mt-4 md:mt-0 bg-[#40E0D0] border-2 border-black rounded-lg 
+               md:rounded-none md:border-l-2 md:border-y-0 md:border-r-0 
+               flex items-center justify-center px-8 py-4 
+               transition-colors duration-200 ease-in-out
+               hover:bg-[#36c7b9] group"
       >
-        <Icon name="heroicons:magnifying-glass" class="w-6 h-6 text-black" />
+        <Icon 
+          name="heroicons:magnifying-glass" 
+          class="w-6 h-6 text-black transition-transform duration-200 group-hover:scale-110" 
+        />
       </button>
 
     </form>
@@ -24,10 +33,7 @@
 </template>
 
 <script setup lang="js">
-
 const handleSearch = () => {
-  // On ajoutera la logique de recherche plus tard
   console.log('Recherche lancée...')
 }
-
 </script>
