53db4a4cd310df4d0208c957439300c4bec574d8
Dylan Tettarasar <dtettarasar@gmail.com>
2026-01-01 12:57:34 +0100

    test carrousel: add style rules

--- nuxt-app/components/category-content/test-carrousel.vue
+++ nuxt-app/components/category-content/test-carrousel.vue
@@ -17,3 +17,34 @@ import { ref } from 'vue'
 const cards = ref([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
 </script>
+
+<style>
+.carousel {
+  width: 170px; /* ❶ */
+  overflow: hidden; /* ❷ */
+}
+
+.inner {
+  white-space: nowrap; /* ❸ */
+}
+
+.card {
+  width: 40px;
+  margin-right: 10px;
+  display: inline-flex;
+
+  /* optional */
+  height: 40px;
+  background-color: #39b1bd;
+  color: white;
+  border-radius: 4px;
+  align-items: center;
+  justify-content: center;
+}
+
+/* optional */
+button {
+  margin-right: 5px;
+  margin-top: 10px;
+}
+</style>
