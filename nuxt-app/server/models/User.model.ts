5d39630471b80888f72089ba8f09fa52c71515ad
Dylan Tettarasar <dtettarasar@gmail.com>
2026-01-20 16:17:09 +0100

    create the user model

---
+++ nuxt-app/server/models/User.model.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { 
+    type: String, 
+    enum: ['user', 'admin'], 
+    default: 'user' 
+  },
+}, { 
+  timestamps: true 
+})
+
+// On exporte avec l'astuce Nuxt pour éviter les erreurs au rechargement
+export const User = mongoose.models.User || mongoose.model('User', UserSchema)
