48eb6bd43e4d333bd02d0f2f517497b8bc933eea
Dylan Tettarasar <dtettarasar@gmail.com>
2026-01-21 18:30:12 +0100

    building the template for the admin login form

--- nuxt-app/pages/admin/login.vue
+++ nuxt-app/pages/admin/login.vue
@@ -1,19 +1,8 @@
 <template>
+
   <div class="container mx-auto">
-    <h1 class="text-2xl font-bold mb-4">Admin Login</h1>
+    <h1 class="text-3xl font-bold mb-8 text-center mt-4">Admin Login</h1>
     <admin-login-form></admin-login-form>
   </div>
+
 </template>
-
-<script setup lang="ts">
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
-
-const email = ref('')
-const password = ref('')
-const router = useRouter()
-
-const login = async () => {
-  // Implement login logic here
-}
-</script>
