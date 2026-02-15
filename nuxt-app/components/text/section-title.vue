f3f8200ea747454bfd2de7ae3df62100e7fd7cf6
Dylan Tettarasar <dtettarasar@gmail.com>
2026-02-15 12:13:30 +0100

    section title component: update the background rules

--- nuxt-app/components/text/section-title.vue
+++ nuxt-app/components/text/section-title.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="md:text-2xl lg:text-4xl font-bold bg-blue-500 inline-block">
-        <h2>Section title</h2>
+    <div class="md:text-2xl lg:text-5xl font-bold inline-block bg-gradient-to-t from-blue-500 from-50% to-transparent to-50%">
+        <h2>Top Stories</h2>
     </div>
 </template>
