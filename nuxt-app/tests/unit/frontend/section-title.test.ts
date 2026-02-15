99cce776e8ad0826c393973444b2b565e353520b
Dylan Tettarasar <dtettarasar@gmail.com>
2026-02-15 16:10:18 +0100

    building test file for the section title component

--- nuxt-app/tests/unit/frontend/section-title.test.ts
+++ nuxt-app/tests/unit/frontend/section-title.test.ts
@@ -1,3 +1,31 @@
 import { mount } from '@vue/test-utils'
 import { describe, it, expect, vi } from 'vitest'
 import DOMPurify from "isomorphic-dompurify"
+
+import sectionTitle from '@/components/text/section-title.vue'
+
+describe("unit test: section-title.vue", () => {
+
+    it('display the default title', async () => {
+
+        const wrapper = mount(sectionTitle)
+
+        await wrapper.vm.$nextTick()
+
+        expect(wrapper.text()).toContain("Section Title")
+
+    })
+
+    it('display a correct title passed as props', async () => {
+        const wrapper = mount(sectionTitle, {
+            props: {
+                text: 'This is a custom title'
+            }
+        })
+    
+        await wrapper.vm.$nextTick()
+    
+        expect(wrapper.text()).toContain('This is a custom title')
+    })
+
+})
