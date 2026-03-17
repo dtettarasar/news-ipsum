24d5ae3e586158479bb3c9ad73cadf371fa48fba
Dylan Tettarasar <dtettarasar@gmail.com>
2026-03-17 13:03:36 +0100

    us-005: created the most popular card component

--- nuxt-app/components/article/MostPopular.vue
+++ nuxt-app/components/article/MostPopular.vue
@@ -4,7 +4,24 @@
         <text-section-title text="Most Popular" backgroundColor="#90f6b7" ></text-section-title>
     </div>
 
+    <div v-if="!isReady" class="mt-6 h-[300px] flex items-center justify-center bg-gray-100 font-bold uppercase tracking-widest">
+        Chargement des articles...
+    </div>
 
+    <div v-else class="mt-6 grid p-2 sm:grid-cols-1 md:p-0 md:grid-cols-2 gap-6">
+        <article-popular-card
+            v-for="article in popular"
+            :key="article._id"
+            :title="article.title"
+            :slug="article.slug"
+            :image="article.image"
+            :category="article.category"
+            :author-name="article.author.name"
+            :author-avatar="article.author.avatar"
+            :read-time="article.readTime"
+            :views="article.views"
+        />
+    </div>
 
 </template>
 
@@ -24,5 +41,7 @@ await useAsyncData('mostPopular', async () => {
 })
 
 console.log('Most Popular Articles:', popular.value)
+
+isReady.value = popular.value && popular.value.length > 0
 
 </script>
