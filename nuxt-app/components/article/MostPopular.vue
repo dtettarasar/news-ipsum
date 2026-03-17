3c6d94b0a35d88c960f34e2ec456a3ae7a1cb26c
Dylan Tettarasar <dtettarasar@gmail.com>
2026-03-17 11:28:34 +0100

    us-005: update script to get the article datas from the store

--- nuxt-app/components/article/MostPopular.vue
+++ nuxt-app/components/article/MostPopular.vue
@@ -1,5 +1,28 @@
 <template>
 
-    placeholder for most popular articles
+    <div>
+        <text-section-title text="Most Popular" backgroundColor="#90f6b7" ></text-section-title>
+    </div>
+
+
 
 </template>
+
+<script setup lang="ts">
+
+import { useArticlesStore } from '@/stores/articlesStore'
+import { storeToRefs } from 'pinia'
+
+const articlesStore = useArticlesStore()
+const { popular } = storeToRefs(articlesStore)
+
+const isReady = ref(false)
+
+await useAsyncData('mostPopular', async () => {
+  await articlesStore.fetchPopular(6)
+  return popular.value
+})
+
+console.log('Most Popular Articles:', popular.value)
+
+</script>
