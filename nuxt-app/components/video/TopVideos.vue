e6d5eb86cd88b659ddaf1ec8f026c104316d18c8
Dylan Tettarasar <dtettarasar@gmail.com>
2026-04-20 16:38:40 +0200

    top video component: loaded the pinia store to retrive article datas

--- nuxt-app/components/video/TopVideos.vue
+++ nuxt-app/components/video/TopVideos.vue
@@ -8,4 +8,21 @@
 
 <script setup lang="ts">
 
+  import { useVideosStore } from '@/stores/videosStore'
+  import { storeToRefs } from 'pinia'
+
+  const videosStore = useVideosStore()
+  const isReady = ref(false)
+
+  const { topVideos } = storeToRefs(videosStore)
+
+  await useAsyncData('topVideos', async () => {
+    await videosStore.fetchTopVideos(5)
+    return topVideos.value
+  })
+
+  isReady.value = topVideos.value && topVideos.value.length > 0
+
+  console.log('Top Videos:', topVideos.value)
+
 </script>
